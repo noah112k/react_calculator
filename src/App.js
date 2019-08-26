ed802b58b302d6fbc5eb14f5ab4c66983d998065
noah112k <50031627+noah112k@users.noreply.github.com>
2019-08-25 19:05:49 -0700

    Made calculator functional and fixed button appearance

--- src/App.js
+++ src/App.js
@@ -5,32 +5,160 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-    display: "0"
+    display: "0",
+    hasOperator: false,
+    operationComplete: false
   };
 
   numClick = num => {
-    if (this.state.display !== "0") {
+    if ((this.state.display === "0" && !this.state.hasOperator) || (this.state.operationComplete)) {
       this.setState({
-        display: this.state.display + num
+        display: num,
+        hasOperator: false,
+        operationComplete: false
       });
-    } else {
+    } else if (this.state.display !== "0" && !this.state.hasOperator) {
       this.setState({
-        display: num
+        display: this.state.display + num,
+        hasOperator: false
+      });
+    } else if (this.state.hasOperator) {
+      this.setState({
+        hasOperator: true,
+        operator: this.state.operator,
+        firstNum: this.state.firstNum,
+        display: this.state.display !== "0" ? this.state.display + num : num
       });
     }
   };
 
   clear = () => {
+    if (this.state.hasOperator) {
+      switch (this.state.operator) {
+        case "+":
+          document.querySelector(".add").classList.remove("operator-selected");
+          break;
+        case "-":
+          document.querySelector(".sub").classList.remove("operator-selected");
+          break;
+        case "*":
+          document.querySelector(".mult").classList.remove("operator-selected");
+          break;
+        case "/":
+          document.querySelector(".div").classList.remove("operator-selected");
+          break;
+      }
+    }
     this.setState({
-      display: "0"
+      display: "0",
+      hasOperator: false,
+      operationComplete: false
     });
+  };
+
+  dotClick = () => {
+    if (!this.state.display.includes(".")) {
+      this.setState({
+        display: this.state.display + ".",
+        hasOperator: false
+      });
+    }
+  };
+
+  opClick = op => {
+    if (this.state.hasOperator) {
+      switch (this.state.operator) {
+        case "+":
+          document.querySelector(".add").classList.remove("operator-selected");
+          break;
+        case "-":
+          document.querySelector(".sub").classList.remove("operator-selected");
+          break;
+        case "*":
+          document.querySelector(".mult").classList.remove("operator-selected");
+          break;
+        case "/":
+          document.querySelector(".div").classList.remove("operator-selected");
+          break;
+      }
+    }
+    switch (op) {
+      case "+":
+        document.querySelector(".add").classList.add("operator-selected");
+        break;
+      case "-":
+        document.querySelector(".sub").classList.add("operator-selected");
+        break;
+      case "*":
+        document.querySelector(".mult").classList.add("operator-selected");
+        break;
+      case "/":
+        document.querySelector(".div").classList.add("operator-selected");
+        break;
+    }
+
+    this.setState({
+      firstNum: this.state.display,
+      operator: op,
+      display: "0",
+      hasOperator: true
+    });
+  };
+
+  equal = () => {
+    if (this.state.hasOperator) {
+      let result = 0;
+      switch (this.state.operator) {
+        case "+":
+          result =
+            parseFloat(this.state.firstNum) + parseFloat(this.state.display);
+          break;
+        case "-":
+          result =
+            parseFloat(this.state.firstNum) - parseFloat(this.state.display);
+          break;
+        case "*":
+          result =
+            parseFloat(this.state.firstNum) * parseFloat(this.state.display);
+          break;
+        case "/":
+          result =
+            parseFloat(this.state.firstNum) / parseFloat(this.state.display);
+          break;
+      }
+      this.setState({
+        display: result,
+        hasOperator: false,
+        operationComplete: true
+      });
+      switch (this.state.operator) {
+        case "+":
+          document.querySelector(".add").classList.remove("operator-selected");
+          break;
+        case "-":
+          document.querySelector(".sub").classList.remove("operator-selected");
+          break;
+        case "*":
+          document.querySelector(".mult").classList.remove("operator-selected");
+          break;
+        case "/":
+          document.querySelector(".div").classList.remove("operator-selected");
+          break;
+      }
+    }
   };
 
   render() {
     return (
       <div className="App">
         <Display display={this.state.display} />
-        <Buttons numClick={this.numClick} clear={this.clear} />
+        <Buttons
+          numClick={this.numClick}
+          clear={this.clear}
+          dotClick={this.dotClick}
+          opClick={this.opClick}
+          equal={this.equal}
+        />
       </div>
     );
   }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,42 +1,131 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Buttons extends Component {
-    render() {
-        return (
-            <div className = "buttons-container">
-                <div onClick = {this.props.clear} style = {opStyle}>C</div>
-                <div style = {opStyle}>+</div>
-                <div style = {opStyle}>-</div>
-                <div style = {opStyle}>/</div>
-                <div style = {opStyle}>*</div>
-                <div style = {opStyle}>=</div>
-                <div onClick = {this.props.numClick.bind(this, '9')}style = {numStyle}>9</div>
-                <div onClick = {this.props.numClick.bind(this, '8')}style = {numStyle}>8</div>
-                <div onClick = {this.props.numClick.bind(this, '7')}style = {numStyle}>7</div>
-                <div onClick = {this.props.numClick.bind(this, '6')}style = {numStyle}>6</div>
-                <div onClick = {this.props.numClick.bind(this, '5')}style = {numStyle}>5</div>
-                <div onClick = {this.props.numClick.bind(this, '4')}style = {numStyle}>4</div>
-                <div onClick = {this.props.numClick.bind(this, '3')}style = {numStyle}>3</div>
-                <div onClick = {this.props.numClick.bind(this, '2')}style = {numStyle}>2</div>
-                <div onClick = {this.props.numClick.bind(this, '1')}style = {numStyle}>1</div>
-                <div onClick = {this.props.numClick.bind(this, '0')}style = {numStyle}>0</div>
-                <div style = {numStyle}>.</div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="buttons-container">
+        <div onClick={this.props.clear} style={opStyle} className="c">
+          C
+        </div>
+        <div
+          onClick={this.props.opClick.bind(this, "+")}
+          style={opStyle}
+          className="add"
+        >
+          +
+        </div>
+        <div
+          onClick={this.props.opClick.bind(this, "-")}
+          style={opStyle}
+          className="sub"
+        >
+          -
+        </div>
+        <div
+          onClick={this.props.opClick.bind(this, "/")}
+          style={opStyle}
+          className="div"
+        >
+          /
+        </div>
+        <div
+          onClick={this.props.opClick.bind(this, "*")}
+          style={opStyle}
+          className="mult"
+        >
+          *
+        </div>
+        <div onClick={this.props.equal} style={opStyle} className="equal">
+          =
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "9")}
+          style={numStyle}
+          className="nine"
+        >
+          9
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "8")}
+          style={numStyle}
+          className="eight"
+        >
+          8
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "7")}
+          style={numStyle}
+          className="seven"
+        >
+          7
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "6")}
+          style={numStyle}
+          className="six"
+        >
+          6
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "5")}
+          style={numStyle}
+          className="five"
+        >
+          5
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "4")}
+          style={numStyle}
+          className="four"
+        >
+          4
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "3")}
+          style={numStyle}
+          className="three"
+        >
+          3
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "2")}
+          style={numStyle}
+          className="two"
+        >
+          2
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "1")}
+          style={numStyle}
+          className="one"
+        >
+          1
+        </div>
+        <div
+          onClick={this.props.numClick.bind(this, "0")}
+          style={numStyle}
+          className="zero"
+        >
+          0
+        </div>
+        <div onClick={this.props.dotClick} style={numStyle} className="dot">
+          .
+        </div>
+      </div>
+    );
+  }
 }
 
 const numStyle = {
-    color: '#000',
-    padding: '20px',
-    backgroundColor: '#ADEFD1',
-    cursor: 'pointer'
-}
+  color: "#000",
+  padding: "20px",
+  backgroundColor: "#ADEFD1",
+  cursor: "pointer"
+};
 const opStyle = {
-    color: '#fff',
-    padding: '20px',
-    backgroundColor: '#00203F',
-    cursor: 'pointer'
-}
-export default Buttons
+  color: "#fff",
+  padding: "20px",
+  backgroundColor: "#00203F",
+  cursor: "pointer"
+};
+export default Buttons;
